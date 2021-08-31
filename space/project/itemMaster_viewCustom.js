32e19b1d2a10e90c3a0017420c2b86f9529593de
kintone-mng <kintone_mng@accel-lab.com>
2021-08-31 12:18:28 +0900

    Update itemMaster_viewCustom.js

--- space/project/itemMaster_viewCustom.js
+++ space/project/itemMaster_viewCustom.js
@@ -18,13 +18,12 @@
     $('.recordlist-edit-gaia').remove();
     $('.recordlist-remove-gaia').remove();
     $('.gaia-argoui-app-menu-edit').remove();
+    $('.gaia-argoui-app-menu-copy').remove();
 
     return event;
   });
 
-  kintone.events.on(['app.record.edit.show'], function(event){
-    $('.gaia-argoui-app-menu-edit').remove();
-    
+  kintone.events.on(['app.record.edit.show'], function(event){    
     event.record.mName.disabled = true;
     event.record.mCode.disabled = true;
     event.record.mNickname.disabled = true;
