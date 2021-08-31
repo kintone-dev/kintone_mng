c8e84a0d0e7007ebc4b8070f6cf68b30957edb4f
kintone-mng <kintone_mng@accel-lab.com>
2021-08-31 10:20:35 +0900

    Update device_adp.js

--- space/inventory/device_adp.js
+++ space/inventory/device_adp.js
@@ -5,7 +5,7 @@
     'app': sysID.DIPM.app.unit,
     'query': null
   };
-  console.log(sysid.INV.app.unit);
+  console.log(sysid.INV.app_id.unit);
   //拠点情報取得＆繰り返し利用
   var getUNITdata=kintone.api(kintone.api.url('/k/v1/records', true), 'GET', getUNITrecord);
   
