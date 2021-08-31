3a13bc346ddae7927868e259ef285e14c350c2ef
kintone-mng <kintone_mng@accel-lab.com>
2021-08-31 17:14:19 +0900

    Update shippingList_adp.js

--- space/inventory/shippingList_adp.js
+++ space/inventory/shippingList_adp.js
@@ -5,14 +5,18 @@
     var nStatus = event.nextStatus.value;
     if(nStatus==="集荷待ち"){
       
-      //パラメータsNumInfoにjsonデータ作成
-      var sNumInfo={'app': sysid.DEV.app.sNum, 'records': []};
-      
-      var shipTable=event.record.deviceList.value;
-      var shipInstName=event.record.instName.value;
-      var shipShipment=event.record.shipment.value;
             
       if(shipShipment === '矢倉倉庫'){
+        //パラメータsNumInfoにjsonデータ作成
+        var sNumInfo={
+          'app': sysid.DEV.app.sNum, 
+          'records': []
+        };
+        
+        var shipTable=event.record.deviceList.value;
+        var shipInstName=event.record.instName.value;
+        var shipShipment=event.record.shipment.value;
+
         for (var i in shipTable){
           var ship_mcode=shipTable[i].value.mCode.value;
           var ship_shipnum=shipTable[i].value.shipNum.value;
@@ -35,6 +39,16 @@
   
         var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'POST', sNumInfo);
       }else{
+        //パラメータsNumInfoにjsonデータ作成
+        var sNumInfo={
+          'app': sysid.DEV.app.sNum, 
+          'records': []
+        };
+        
+        var shipTable=event.record.deviceList.value;
+        var shipInstName=event.record.instName.value;
+        var shipShipment=event.record.shipment.value;
+
         for (var i in shipTable){
           var ship_mcode=shipTable[i].value.mCode.value;
           var ship_shipnum=shipTable[i].value.shipNum.value;
@@ -45,6 +59,10 @@
           var sNums=get_sNums.filter(Boolean);
           
           for(var y in sNums){
+            var updateKey={
+                'field': 'sNum',
+                'value': sNums[y]     
+            }
             var snRecord={
               'sNum':{'value':sNums[y]},
               'mCode':{'value':ship_mcode},
@@ -54,6 +72,8 @@
             sNumInfo.records.push(snRecord);
           }
         }
+
+        console.log(sNumInfo);
   
         var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
       }
