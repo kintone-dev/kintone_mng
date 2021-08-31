d7aeeb4da7d5dc9310b817af4e8af8d4e0dde49d
kintone-mng <kintone_mng@accel-lab.com>
2021-08-31 18:06:48 +0900

    Update shippingList_adp.js

--- space/inventory/shippingList_adp.js
+++ space/inventory/shippingList_adp.js
@@ -4,81 +4,82 @@
   var getSNUMdata=api_getRecords(sysid.DEV.app.sNum);
 
   kintone.events.on('app.record.detail.process.proceed',function(event){
-    getSNUMdata.then(function(resp){
-      var tarRecords=resp.records;
-      var nStatus = event.nextStatus.value;
-
-      if(nStatus==="集荷待ち"){
-        //パラメータsNumInfoにjsonデータ作成
-        var sNumInfo={
-          'app': sysid.DEV.app.sNum, 
-          'records': []
-        };
+    var tarRecords=resp.records;
+    var nStatus = event.nextStatus.value;
+    
+    if(nStatus==="集荷待ち"){
+      //パラメータsNumInfoにjsonデータ作成
+      var sNumInfo={
+        'app': sysid.DEV.app.sNum, 
+        'records': []
+      };
+      
+      var shipTable=event.record.deviceList.value;
+      var shipInstName=event.record.instName.value;
+      var shipShipment=event.record.shipment.value;
+      
+      
+      if(shipShipment === '矢倉倉庫'){
+        for (var i in shipTable){
+          var ship_mcode=shipTable[i].value.mCode.value;
+          var ship_shipnum=shipTable[i].value.shipNum.value;
+          var ship_sn=shipTable[i].value.sNum.value;
+          //get serial numbers
+          var get_sNums=ship_sn.split(/\r\n|\n/);
+          //except Boolean
+          var sNums=get_sNums.filter(Boolean);
+          
+          for(var y in sNums){
+            var snRecord={
+              'sNum':{'value':sNums[y]},
+              'mCode':{'value':ship_mcode},
+              'instName':{'value':shipInstName},
+              'shipment':{'value':shipShipment}
+            };
+            sNumInfo.records.push(snRecord);
+          }
+        }
         
-        var shipTable=event.record.deviceList.value;
-        var shipInstName=event.record.instName.value;
-        var shipShipment=event.record.shipment.value;
-  
-  
-        if(shipShipment === '矢倉倉庫'){
-          for (var i in shipTable){
-            var ship_mcode=shipTable[i].value.mCode.value;
-            var ship_shipnum=shipTable[i].value.shipNum.value;
-            var ship_sn=shipTable[i].value.sNum.value;
-            //get serial numbers
-            var get_sNums=ship_sn.split(/\r\n|\n/);
-            //except Boolean
-            var sNums=get_sNums.filter(Boolean);
-            
-            for(var y in sNums){
-              var snRecord={
+        var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'POST', sNumInfo);
+      }else{
+        for (var i in shipTable){
+          var ship_mcode=shipTable[i].value.mCode.value;
+          var ship_shipnum=shipTable[i].value.shipNum.value;
+          var ship_sn=shipTable[i].value.sNum.value;
+          //get serial numbers
+          var get_sNums=ship_sn.split(/\r\n|\n/);
+          //except Boolean
+          var sNums=get_sNums.filter(Boolean);
+          
+          for(var y in sNums){      
+            var snRecord={
+              // 'id': tarRecords[y].getId(),
+              'updatekey':{
+                'field':'sNum',
+                'value':sNums[y]
+              },
+              'record':{
                 'sNum':{'value':sNums[y]},
                 'mCode':{'value':ship_mcode},
                 'instName':{'value':shipInstName},
                 'shipment':{'value':shipShipment}
-              };
-              sNumInfo.records.push(snRecord);
-            }
+              }
+            };
+            sNumInfo.records.push(snRecord);
           }
-    
-          var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'POST', sNumInfo);
-        }else{
-          for (var i in shipTable){
-            var ship_mcode=shipTable[i].value.mCode.value;
-            var ship_shipnum=shipTable[i].value.shipNum.value;
-            var ship_sn=shipTable[i].value.sNum.value;
-            //get serial numbers
-            var get_sNums=ship_sn.split(/\r\n|\n/);
-            //except Boolean
-            var sNums=get_sNums.filter(Boolean);
-            
-            for(var y in sNums){      
-              var snRecord={
-                'id': tarRecords[y].getId(),
-                'record':{
-                  'sNum':{'value':sNums[y]},
-                  'mCode':{'value':ship_mcode},
-                  'instName':{'value':shipInstName},
-                  'shipment':{'value':shipShipment}
-                }
-              };
-              sNumInfo.records.push(snRecord);
-            }
-          }
-  
-          console.log(sNumInfo);
-    
-          var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
         }
-  
-        return setSNinfo.then(function(resp){
-          alert('update success');
-        }).catch(function(error){
-          alert('update error'+error.message);
-          console.log(error)
-        });
+        
+        console.log(sNumInfo);
+        
+        var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
       }
       
-    });
+      return setSNinfo.then(function(resp){
+        alert('update success');
+      }).catch(function(error){
+        alert('update error'+error.message);
+        console.log(error)
+      });
+    }
   });
 })();
