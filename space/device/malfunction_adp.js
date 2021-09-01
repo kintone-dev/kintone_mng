97fb2083c7e96e47de133f6944142a2d68f8213d
kintone-mng <kintone_mng@accel-lab.com>
2021-09-01 13:43:14 +0900

    Update malfunction_adp.js

--- space/device/malfunction_adp.js
+++ space/device/malfunction_adp.js
@@ -43,7 +43,8 @@
 
     var queryBody = {
       'app': sysid.DEV.app.sNum,
-      'query': 'sNum="' + event.record.defective.value + '"',
+      'sNum': event.record.defective.value
+      // 'query': 'sNum="' + event.record.defective.value + '"',
     };
 
     var getresult = kintone.api(kintone.api.url('/k/v1/record', true), 'GET', queryBody);
