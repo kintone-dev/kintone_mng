030e68eceda8f0998368a93babec176a0eddfac1
kintone-mng <kintone_mng@accel-lab.com>
2021-09-01 12:28:43 +0900

    Update malfunction_adp.js

--- space/device/malfunction_adp.js
+++ space/device/malfunction_adp.js
@@ -9,23 +9,23 @@
   function createPutRecords(records) {
     var putRecords = [];
     for (var i = 0, l = records.length; i < l; i++) {
-        var record = records[i];
-        putRecords[i] = {
-            id: record.$id.value,
-            record: {
-                lookup: {
-                    value: record.lookup.value
-                }
-            }
-        };
+      var record = records[i];
+      putRecords[i] = {
+        id: record.$id.value,
+        record: {
+          lookup: {
+            value: record.lookup.value
+          }
+        }
+      };
     }
     return putRecords;
-}
+  }
 
 
   kintone.events.on(events_ced, function (event) {
 
-    var snMalfunction = event.record.malfunction.value;
+    var snDefective = event.record.defective.value;
 
     var sNumInfo = {
       'app': sysid.DEV.app.sNum,
@@ -35,11 +35,15 @@
     var snRecord = {
       'updateKey': {
         'field': 'sNum',
-        'value': snMalfunction
+        'value': snDefective
       },
       'record': {
-        'sState':{'value':'故障品'},
-        'sProgress':{'value':'検証待ち'}
+        'sState': {
+          'value': '故障品'
+        },
+        'sProgress': {
+          'value': '検証待ち'
+        }
       }
     };
 
@@ -47,15 +51,15 @@
 
     var putResult = kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
 
-    putResult.then(function(resp){
+    putResult.then(function (resp) {
       console.log(resp);
-    }).catch(function(error){
+    }).catch(function (error) {
       console.error(error);
     });
 
     var queryBody = {
       'app': sysid.DEV.app.sNum,
-      'query': 'sNum="' + event.record.malfunction.value +'"',
+      'query': 'sNum="' + event.record.malfunction.value + '"',
     };
 
     var getresult = kintone.api(kintone.api.url('/k/v1/records', true), 'GET', queryBody);
@@ -64,12 +68,12 @@
 
       var records = resp.records;
       var paramPut = {
-          'app': kintone.app.getId(),
-          'records': createPutRecords(records)
+        'app': kintone.app.getId(),
+        'records': createPutRecords(records)
       };
-      
+
       kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', paramPut);
-      
+
     }).catch(function (error) {
       console.log(error);
       console.log(error.message);
