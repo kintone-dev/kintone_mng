c9e4c84ed88a40a08d44b607bab415ba3cde5f71
kintone-mng <kintone_mng@accel-lab.com>
2021-09-02 11:47:35 +0900

    Update shippingList_adp.js

--- space/inventory/shippingList_adp.js
+++ space/inventory/shippingList_adp.js
@@ -92,14 +92,12 @@
 
   kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
     setBtn('calBtn', '計算');
-    
-    console.log(event);
 
     $('#calBtn').on('click', function () {
       var eRecord = kintone.app.record.get();
       var shipTable = eRecord.record.deviceList.value;
 
-      eRecord.record.error = 'aaa'
+      kintone.app.error = 'aaa'
 
       var lengthStr = '';
       var openType = '';
