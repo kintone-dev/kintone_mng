85497a48982f8bea54f47798603168c3a2ed5223
kintone-mng <kintone_mng@accel-lab.com>
2021-09-02 17:57:02 +0900

    Create tc_viewCustom.js

---
+++ space/device/tc_viewCustom.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  var events_ced = [
+    'app.record.create.show'
+  ];
+
+  kintone.events.on(events_ced, function (event) {
+
+
+    return event;
+  });
+
+})();
