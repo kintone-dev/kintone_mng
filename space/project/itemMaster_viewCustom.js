684a2e0017b0c85207f5e0dd45f489f41f171c3d
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-02 15:23:08 +0900

    Update itemMaster_viewCustom.js

--- space/project/itemMaster_viewCustom.js
+++ space/project/itemMaster_viewCustom.js
@@ -1,26 +1,12 @@
 (function(){
   'use strict';
 
-  var events_ced=[
-    'app.record.index.show',
-    'app.record.detail.show',
-    'app.record.create.show',
-    'app.record.edit.show',
-    'app.record.print.show',
-    'app.report.show',
-    'portal.show',
-    'space.portal.show'
-  ];
-
   kintone.events.on(events_ced, function(event){
     $('.gaia-argoui-app-menu-add').remove();
     $('.recordlist-edit-gaia').remove();
     $('.recordlist-remove-gaia').remove();
     $('.gaia-argoui-app-menu-edit').remove();
     $('.gaia-argoui-app-menu-copy').remove();
-
-    return event;
-  });
 
   kintone.events.on(['app.record.edit.show'], function(event){    
     event.record.mName.disabled = true;
