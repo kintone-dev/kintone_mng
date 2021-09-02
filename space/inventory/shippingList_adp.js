032bb45d2d4ce05051c6af52a708ff6639f0c145
kintone-mng <kintone_mng@accel-lab.com>
2021-09-02 10:48:17 +0900

    Update shippingList_adp.js

--- space/inventory/shippingList_adp.js
+++ space/inventory/shippingList_adp.js
@@ -1,86 +1,100 @@
-(function() {
+(function () {
   'use strict';
 
   //拠点情報取得＆繰り返し利用
-  kintone.events.on('app.record.detail.process.proceed',function(event){
+  kintone.events.on('app.record.detail.process.proceed', function (event) {
     var nStatus = event.nextStatus.value;
-    
-    if(nStatus==="集荷待ち"){
+
+    if (nStatus === "集荷待ち") {
       //パラメータsNumInfoにjsonデータ作成
-      var sNumInfo={
-        'app': sysid.DEV.app.sNum, 
+      var sNumInfo = {
+        'app': sysid.DEV.app.sNum,
         'records': []
       };
-      
-      var shipTable=event.record.deviceList.value;
-      var shipInstName=event.record.instName.value;
-      var shipShipment=event.record.shipment.value;
-      
-      
-      if(shipShipment === '矢倉倉庫'){
-        for (var i in shipTable){
-          var ship_mcode=shipTable[i].value.mCode.value;
-          var ship_shipnum=shipTable[i].value.shipNum.value;
-          var ship_sn=shipTable[i].value.sNum.value;
+
+      var shipTable = event.record.deviceList.value;
+      var shipInstName = event.record.instName.value;
+      var shipShipment = event.record.shipment.value;
+
+
+      if (shipShipment === '矢倉倉庫') {
+        for (var i in shipTable) {
+          var ship_mcode = shipTable[i].value.mCode.value;
+          var ship_shipnum = shipTable[i].value.shipNum.value;
+          var ship_sn = shipTable[i].value.sNum.value;
           //get serial numbers
-          var get_sNums=ship_sn.split(/\r\n|\n/);
+          var get_sNums = ship_sn.split(/\r\n|\n/);
           //except Boolean
-          var sNums=get_sNums.filter(Boolean);
-          
-          for(var y in sNums){
-            var snRecord={
-              'sNum':{'value':sNums[y]},
-              'mCode':{'value':ship_mcode},
-              'instName':{'value':shipInstName},
-              'shipment':{'value':shipShipment}
-            };
-            sNumInfo.records.push(snRecord);
-          }
-        }
-        
-        var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'POST', sNumInfo);
-      }else{
-        for (var i in shipTable){
-          var ship_mcode=shipTable[i].value.mCode.value;
-          var ship_shipnum=shipTable[i].value.shipNum.value;
-          var ship_sn=shipTable[i].value.sNum.value;
-          //get serial numbers
-          var get_sNums=ship_sn.split(/\r\n|\n/);
-          //except Boolean
-          var sNums=get_sNums.filter(Boolean);
-          
-          for(var y in sNums){      
-            var snRecord={
-              'updateKey':{
-                'field':'sNum',
-                'value':sNums[y]
+          var sNums = get_sNums.filter(Boolean);
+
+          for (var y in sNums) {
+            var snRecord = {
+              'sNum': {
+                'value': sNums[y]
               },
-              'record':{
-                'mCode':{'value':ship_mcode},
-                'instName':{'value':shipInstName},
-                'shipment':{'value':shipShipment}
+              'mCode': {
+                'value': ship_mcode
+              },
+              'instName': {
+                'value': shipInstName
+              },
+              'shipment': {
+                'value': shipShipment
               }
             };
             sNumInfo.records.push(snRecord);
           }
         }
-        
-        var setSNinfo= new kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
+
+        var setSNinfo = new kintone.api(kintone.api.url('/k/v1/records', true), 'POST', sNumInfo);
+      } else {
+        for (var i in shipTable) {
+          var ship_mcode = shipTable[i].value.mCode.value;
+          var ship_shipnum = shipTable[i].value.shipNum.value;
+          var ship_sn = shipTable[i].value.sNum.value;
+          //get serial numbers
+          var get_sNums = ship_sn.split(/\r\n|\n/);
+          //except Boolean
+          var sNums = get_sNums.filter(Boolean);
+
+          for (var y in sNums) {
+            var snRecord = {
+              'updateKey': {
+                'field': 'sNum',
+                'value': sNums[y]
+              },
+              'record': {
+                'mCode': {
+                  'value': ship_mcode
+                },
+                'instName': {
+                  'value': shipInstName
+                },
+                'shipment': {
+                  'value': shipShipment
+                }
+              }
+            };
+            sNumInfo.records.push(snRecord);
+          }
+        }
+
+        var setSNinfo = new kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', sNumInfo);
       }
-      
-      return setSNinfo.then(function(resp){
+
+      return setSNinfo.then(function (resp) {
         console.log(resp);
-      }).catch(function(error){
+      }).catch(function (error) {
         console.error(error);
       });
     }
   });
 
-  kintone.events.on(['app.record.edit.show','app.record.create.show'],function(event){
-    setBtn('calBtn','計算');
+  kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
+    setBtn('calBtn', '計算');
 
 
-    $('#calBtn').on('click', function() {
+    $('#calBtn').on('click', function () {
       var eRecord = kintone.app.record.get();
       var shipTable = eRecord.record.deviceList.value;
 
@@ -90,30 +104,34 @@
       var shipNum = '';
 
       var numRegExp = new RegExp(/^([1-9]\d*|0)$/);
-      var openRegExp = new RegExp(/[SW]/);
-      var methodRegExp = new RegExp(/壁づけ|天井/);
+      var openRegExp = new RegExp(/[swSW]/);
+      var methodRegExp = new RegExp(/壁付S|壁付W|天井/);
 
       var railSpecs = (String(shipTable[0].value.sNum.value)).split(/,\n/);
 
-      for(var i in railSpecs){
-        if(numRegExp.test(railSpecs[i])){
+      for (var i in railSpecs) {
+        if (numRegExp.test(railSpecs[i])) {
           lengthStr = railSpecs[i];
+        } else {
+          shipTable[0].value.sNum.error = '数字を入力してください';
         }
 
-        if(openRegExp.test(railSpecs[i])){
+        if (openRegExp.test(railSpecs[i])) {
           openType = railSpecs[i];
+        } else {
+          shipTable[0].value.sNum.error = 'SかWで入力してください';
         }
-        
-        if(methodRegExp.test(railSpecs[i])){
-          if( railSpecs[i].match(/壁づけ/)){
-            methodType = '壁づけ';
+
+        if (methodRegExp.test(railSpecs[i])) {
+          if (railSpecs[i].match(/壁付/)) {
+            methodType = '壁付';
           } else {
             methodType = '天井';
           }
         }
       }
 
-      if(numRegExp.test(shipTable[0].value.shipNum.value)){
+      if (numRegExp.test(shipTable[0].value.shipNum.value)) {
         shipNum = shipTable[0].value.shipNum.value;
       }
 
