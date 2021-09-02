e0df47e9bf9b2bd5864cb0b4b520903a66ad086c
kintone-mng <kintone_mng@accel-lab.com>
2021-09-02 12:31:00 +0900

    Update shippingList_adp.js

--- space/inventory/shippingList_adp.js
+++ space/inventory/shippingList_adp.js
@@ -93,71 +93,70 @@
   kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
     setBtn('calBtn', '計算');
 
+    $('#calBtn').on('click', function () {
+      var eRecord = kintone.app.record.get();
+      var shipTable = eRecord.record.deviceList.value;
+    
+      var lengthStr = '';
+      var openType = '';
+      var methodType = '';
+      var shipNum = '';
+  
+      var numRegExp = new RegExp(/^([1-9]\d*|0)$/);
+      var openRegExp = new RegExp(/^[sw]/i);
+      var methodRegExp = new RegExp(/壁付[sw]|天井/i);
+  
+      var railSpecs = (String(shipTable[0].value.sNum.value)).split(/,\n|\n/);
+  
+      for (var i in railSpecs) {
+        if (numRegExp.test(railSpecs[i])) {
+          lengthStr = railSpecs[i];
+          shipTable[0].value.sNum.error = null;
+        } else {
+          shipTable[0].value.sNum.error = '入力形式が間違えています';
+        }
+  
+        if (openRegExp.test(railSpecs[i])) {
+          if(railSpecs[i].length === 1){
+            openType = railSpecs[i];
+            shipTable[0].value.sNum.error = null;
+          } else{
+            shipTable[0].value.sNum.error = '入力形式が間違えています';
+          }
+        } else {
+          shipTable[0].value.sNum.error = '入力形式が間違えています';
+        }
+  
+        if (methodRegExp.test(railSpecs[i])) {
+          if (railSpecs[i].match(/壁付s/i)) {
+            methodType = '壁付S';
+          } else if (railSpecs[i].match(/壁付w/i)) {
+            methodType = '壁付W';
+          } else {
+            methodType = '天井';
+          }
+          shipTable[0].value.sNum.error = null;
+        } else {
+          shipTable[0].value.sNum.error = '入力形式が間違えています';
+        }
+      }
+  
+      if (numRegExp.test(shipTable[0].value.shipNum.value)) {
+        shipNum = shipTable[0].value.shipNum.value;
+      }
+  
+      console.log(shipNum);
+      console.log(lengthStr);
+      console.log(openType);
+      console.log(methodType);
+      console.log(railSpecs);
+  
+      // trtDY(1,2,3);
+      kintone.app.record.set(eRecord);
+    });
+
     return event;
   });
 
-  $('#calBtn').on('click', function () {
-    var eRecord = kintone.app.record.get();
-    var shipTable = eRecord.record.deviceList.value;
-
-    eRecord.error = 'aaa';
-
-    var lengthStr = '';
-    var openType = '';
-    var methodType = '';
-    var shipNum = '';
-
-    var numRegExp = new RegExp(/^([1-9]\d*|0)$/);
-    var openRegExp = new RegExp(/^[sw]/i);
-    var methodRegExp = new RegExp(/壁付[sw]|天井/i);
-
-    var railSpecs = (String(shipTable[0].value.sNum.value)).split(/,\n|\n/);
-
-    for (var i in railSpecs) {
-      if (numRegExp.test(railSpecs[i])) {
-        lengthStr = railSpecs[i];
-        shipTable[0].value.sNum.error = null;
-      } else {
-        shipTable[0].value.sNum.error = '入力形式が間違えています';
-      }
-
-      if (openRegExp.test(railSpecs[i])) {
-        if(railSpecs[i].length === 1){
-          openType = railSpecs[i];
-          shipTable[0].value.sNum.error = null;
-        } else{
-          shipTable[0].value.sNum.error = '入力形式が間違えています';
-        }
-      } else {
-        shipTable[0].value.sNum.error = '入力形式が間違えています';
-      }
-
-      if (methodRegExp.test(railSpecs[i])) {
-        if (railSpecs[i].match(/壁付s/i)) {
-          methodType = '壁付S';
-        } else if (railSpecs[i].match(/壁付w/i)) {
-          methodType = '壁付W';
-        } else {
-          methodType = '天井';
-        }
-        shipTable[0].value.sNum.error = null;
-      } else {
-        shipTable[0].value.sNum.error = '入力形式が間違えています';
-      }
-    }
-
-    if (numRegExp.test(shipTable[0].value.shipNum.value)) {
-      shipNum = shipTable[0].value.shipNum.value;
-    }
-
-    console.log(shipNum);
-    console.log(lengthStr);
-    console.log(openType);
-    console.log(methodType);
-    console.log(railSpecs);
-
-    // trtDY(1,2,3);
-    kintone.app.record.set(eRecord);
-  });
 
 })();
