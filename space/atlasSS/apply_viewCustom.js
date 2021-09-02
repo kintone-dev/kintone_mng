a35bd537c795c3bdc383c8b38633ce0c9d2fc62f
kintone-mng <kintone_mng@accel-lab.com>
2021-09-02 18:18:25 +0900

    Update apply_viewCustom.js

--- space/atlasSS/apply_viewCustom.js
+++ space/atlasSS/apply_viewCustom.js
@@ -3,20 +3,20 @@
   kintone.events.on('app.record.detail.show', function(event){
     var bizPW=event.record.toastcam_bizUserPassword.value;
     
-    var create_bizid=setBtn('btn_create_bizid','bizID作成');
-    $('#'+create_bizid.id).on('click', function(){
-      var menID=event.record.member_id.value;
-      var setRecordBody={
-        'app': kintone.app.getId(),
-        'id': kintone.app.record.getId(),
-        'record': {
-          'toastcam_bizUserId': {'value': menID+'@accel-lab.com'},
-          'toastcam_bizUserPassword': {'value': pw_generator(10)}
-        }
-      };
-      kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', setRecordBody);
-      location.reload();
-    });
+    // var create_bizid=setBtn('btn_create_bizid','bizID作成');
+    // $('#'+create_bizid.id).on('click', function(){
+    //   var menID=event.record.member_id.value;
+    //   var setRecordBody={
+    //     'app': kintone.app.getId(),
+    //     'id': kintone.app.record.getId(),
+    //     'record': {
+    //       'toastcam_bizUserId': {'value': menID+'@accel-lab.com'},
+    //       'toastcam_bizUserPassword': {'value': pw_generator(10)}
+    //     }
+    //   };
+    //   kintone.api(kintone.api.url('/k/v1/record', true), 'PUT', setRecordBody);
+    //   location.reload();
+    // });
     //bizPASSが既にある場合、「bizID作成」ボタン非表示
     var elTag = kintone.app.record.getSpaceElement('btn_create_bizid');
     if(bizPW==='') elTag.parentNode.style.display = 'block';
