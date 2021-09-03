a1c374bf0627b06e0acaf2ab78278ab7dce63a44
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-03 14:57:54 +0900

    Update item_viewCustom.js

--- general/js/item_viewCustom.js
+++ general/js/item_viewCustom.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-
+  // 新規、編集、複製ボタン非表示
   var events_ced=[
     'app.record.index.show',
     'app.record.detail.show',
@@ -11,25 +11,31 @@
     'portal.show',
     'space.portal.show'
   ];
-
   kintone.events.on(events_ced, function(event){
     $('.gaia-argoui-app-menu-add').remove();
     $('.recordlist-edit-gaia').remove();
     $('.recordlist-remove-gaia').remove();
     $('.gaia-argoui-app-menu-edit').remove();
     $('.gaia-argoui-app-menu-copy').remove();
-
     return event;
   });
 
+  // 
   kintone.events.on(['app.record.edit.show'], function(event){    
     event.record.mName.disabled = true;
     event.record.mCode.disabled = true;
     event.record.mNickname.disabled = true;
     event.record.mType.disabled = true;
     event.record.mVendor.disabled = true;
-    event.record.endservice.disabled = true;
-
+    event.record.mClassification.disabled = true;
+    for (var sti in event.record.packageComp.value){
+      event.record.packageComp.value[sti].value.pc_mVendor.disabled=true;
+      event.record.packageComp.value[sti].value.pc_mType.disabled=true;
+      event.record.packageComp.value[sti].value.pc_mCode.disabled=true;
+      event.record.packageComp.value[sti].value.pc_mName.disabled=true;
+      event.record.packageComp.value[sti].value.pc_Num.disabled=true;
+    }
+    [].forEach.call(document.getElementsByClassName("subtable-operation-gaia"), function(button){ button.style.display = 'none'; });
     return event;
   });
   kintone.events.on(['app.record.create.show','app.record.detail.show','app.record.edit.show'], function(event){
