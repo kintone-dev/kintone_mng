353fdc42c7ef1e96a393449e45e863bc4c6f76cd
kintone-mng <kintone_mng@accel-lab.com>
2021-09-06 18:32:58 +0900

    Update delivery-indexCustom.js

--- space/atlasSS/delivery-indexCustom.js
+++ space/atlasSS/delivery-indexCustom.js
@@ -184,15 +184,28 @@
               console.log(putDefData);
               console.log(putRepData);
 
-              postRecords(sysid.ASS.app_id.member, postMemData).catch(function (error) {
+              postRecords(sysid.ASS.app_id.member, postMemData)
+              .then(function (resp) {
+                console.log(resp);
+              }).catch(function (error) {
+                console.log(error);
                 alert('内部情報連携失敗しました。システム管理者に連絡してください。');
               });
-              putRecords(sysid.DEV.app_id.sNum, putDefData).catch(function (error) {
+              putRecords(sysid.DEV.app_id.sNum, putDefData)
+              .then(function (resp) {
+                console.log(resp);
+              }).catch(function (error) {
+                console.log(error);
                 alert('内部情報連携失敗しました。システム管理者に連絡してください。');
               });
-              putRecords(sysid.DEV.app_id.sNum, putRepData).catch(function (error) {
+
+              putRecords(sysid.DEV.app_id.sNum, putRepData).then(function (resp) {
+                console.log(resp);
+              }).catch(function (error) {
+                console.log(error);
                 alert('内部情報連携失敗しました。システム管理者に連絡してください。');
               });
+
               // putRecords(kintone.app.getId(), putWStatData);
 
               // postRecords(sysid.ASS.app_id.member, postMemData);
