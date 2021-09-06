2312a5b1f13a4aa08ef3896e9185a4fda0856316
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-06 19:26:17 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -5,7 +5,8 @@
     return event;
   });
   kintone.events.on(['app.record.create.change.shipType','app.record.edit.change.shipType'], function(event){
-    if(event.record.shipType.value=='拠点間移動') event.record.dstSelection.value=='施工業者/拠点へ納品';
+    if(event.record.shipType.value=='拠点間移動') console.log('test ok');//event.record.dstSelection.value=='施工業者/拠点へ納品';
+    event.record.zipcode.value==123123;
     return event;
   });
   kintone.events.on(['app.record.create.show','app.record.edit.show','app.record.detail.show'], function(event){
