85dbd3668b18685b286c10902b64ed94b102bfee
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-06 19:48:43 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -6,7 +6,7 @@
   });
   kintone.events.on(['app.record.create.change.shipType','app.record.edit.change.shipType'], function(event){
     if(event.record.shipType.value=='拠点間移動') console.log('test ok');//event.record.dstSelection.value=='施工業者/拠点へ納品';
-    event.record.city.value==123123;
+    event.record.city.value=='123123';
     return event;
   });
   kintone.events.on(['app.record.create.show','app.record.edit.show','app.record.detail.show'], function(event){
