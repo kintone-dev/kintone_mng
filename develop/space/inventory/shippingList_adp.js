14040a7f89dcdc18019501646e4a18cb9acca1db
kintone-mng <kintone_mng@accel-lab.com>
2021-09-08 16:34:20 +0900

    Update shippingList_adp.js

--- develop/space/inventory/shippingList_adp.js
+++ develop/space/inventory/shippingList_adp.js
@@ -334,8 +334,8 @@
                 'value': sendDate
               },
               'record': {
-                'inventoryList':{
-                  value:resp.records[0].inventoryList.value
+                'inventoryList': {
+                  value: resp.records[0].inventoryList.value
                 }
               }
             }
@@ -347,58 +347,61 @@
             var inventoryList = resp.records[0].inventoryList.value;
             for (var il in inventoryList) {
               var reportSysData = {
-                'sysCode':inventoryList[il].value.sys_code.value,
-                'rowId':inventoryList[il].id,
-                'shipNum':inventoryList[il].value.shipNum.value
+                'sysCode': inventoryList[il].value.sys_code.value,
+                'rowId': inventoryList[il].id,
+                'shipNum': inventoryList[il].value.shipNum.value
               }
               reportSysCode.push(reportSysData);
             }
-            for(var dl in deviceList){
+            for (var dl in deviceList) {
               var shipSysData = {
-                'sysCode':deviceList[dl].value.mCode.value + '-' + sysUCode,
-                'shipNum':deviceList[dl].value.shipNum.value,
+                'sysCode': deviceList[dl].value.mCode.value + '-' + sysUCode,
+                'shipNum': deviceList[dl].value.shipNum.value,
               }
               var shipDistributeData = {
-                'sysCode':deviceList[dl].value.mCode.value + '-distribute',
-                'shipNum':deviceList[dl].value.shipNum.value,
+                'sysCode': deviceList[dl].value.mCode.value + '-distribute',
+                'shipNum': deviceList[dl].value.shipNum.value,
               }
               shipSysCode.push(shipSysData);
               shipDistributeCode.push(shipDistributeData);
             }
 
-            for(var dl in deviceList){
+            for (var ssc in shipSysCode) {
               //サブテーブル追加
-              if(reportSysCode.some(item => item.sysCode === shipSysCode[dl].sysCode)){
+              if (reportSysCode.some(item => item.sysCode === shipSysCode[ssc].sysCode)) {
                 for (var il in putReportBody.record.inventoryList.value) {
-                  if(putReportBody.record.inventoryList.value[il].value.sys_code.value == shipSysCode[dl].sysCode){
-                    putReportBody.record.inventoryList.value[il].value.shipNum.value = parseInt(putReportBody.record.inventoryList.value[il].value.shipNum.value) + parseInt(deviceList[dl].shipNum)
+                  if (putReportBody.record.inventoryList.value[il].value.sys_code.value == shipSysCode[ssc].sysCode) {
+                    putReportBody.record.inventoryList.value[il].value.shipNum.value = parseInt(putReportBody.record.inventoryList.value[il].value.shipNum.value) + parseInt(shipSysCode[ssc].shipNum)
                   }
                 }
-              }else{
+              } else {
                 var putInventoryBody = {
-                  'value':{
-                    'sys_code':shipSysCode[dl].sysCode,
-                    'stockLocation':PAGE_RECORD.shipment.value,
-                    'shipNum':shipSysCode[dl].shipNum
-                  }
-                }
-                putReportBody.record.inventoryList.value.push(putInventoryBody);
-              }
-              //distribute追加
-              if(reportSysCode.some(item => item.sysCode === shipDistributeCode[dl].sysCode)){
-
-              }else{
-                var putInventoryBody = {
-                  'value':{
-                    'sys_code':shipDistributeCode[dl].sysCode,
-                    'stockLocation':'積送',
-                    'arrivalNum':shipDistributeCode[dl].shipNum
+                  'value': {
+                    'sys_code': shipSysCode[ssc].sysCode,
+                    'stockLocation': PAGE_RECORD.shipment.value,
+                    'shipNum': shipSysCode[ssc].shipNum
                   }
                 }
                 putReportBody.record.inventoryList.value.push(putInventoryBody);
               }
             }
-            
+
+            for (var sdc in shipDistributeCode) {
+              //distribute追加
+              if (reportSysCode.some(item => item.sysCode === shipDistributeCode[sdc].sysCode)) {
+
+              } else {
+                var putInventoryBody = {
+                  'value': {
+                    'sys_code': shipDistributeCode[sdc].sysCode,
+                    'stockLocation': '積送',
+                    'arrivalNum': shipDistributeCode[sdc].shipNum
+                  }
+                }
+                putReportBody.record.inventoryList.value.push(putInventoryBody);
+              }
+            }
+
             putReportData.push(putReportBody);
             console.log(putReportData);
             putRecords(sysid.INV.app_id.report, putReportData);
