bbcdc08da72fb005c1df33822b3b968c95ea2fc8
kintone-mng <kintone_mng@accel-lab.com>
2021-09-08 18:25:23 +0900

    Update report_adp.js

--- develop/space/inventory/report_adp.js
+++ develop/space/inventory/report_adp.js
@@ -8,7 +8,7 @@
       const REPORT_KEY_MONTH = PAGE_RECORD.report_key.value.substring(4, 7);
       var reportDate = new Date(REPORT_KEY_YEAR,REPORT_KEY_MONTH);
       reportDate.setMonth(reportDate.getMonth()+1);
-      const NEXT_DATE = String(reportDate.getFullYear()) + String(reportDate.getMonth());
+      const NEXT_DATE = String(reportDate.getFullYear()) + String(("0"+(date.getMonth() + 1)).slice(-2));
       console.log(NEXT_DATE);
       var getNextMonthReportBody = {
         'app': sysid.INV.app_id.report,
