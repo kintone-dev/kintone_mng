57b4f5fc8b372de93e5438444a9cefbb4dcd55b8
kintone-mng <kintone_mng@accel-lab.com>
2021-09-08 17:55:47 +0900

    Update report_adp.js

--- develop/space/inventory/report_adp.js
+++ develop/space/inventory/report_adp.js
@@ -4,13 +4,15 @@
   kintone.events.on(['app.record.edit.submit.success', 'app.record.create.submit.success'], function (event) {
     const PAGE_RECORD = event.record;
     if (PAGE_RECORD.EoMcheck.value == '締切') {
-      const REPORT_KEY = event.record.report_key.value;
-      var repordData = new Date(REPORT_KEY);
-      console.log(repordData);
+      const REPORT_KEY_YEAR = PAGE_RECORD.report_key.value.substring(0, 4);
+      const REPORT_KEY_MONTH = REPORT_KEY.report_key.value.substring(5, 7);
+      console.log(REPORT_KEY_YEAR);
+      console.log(REPORT_KEY_MONTH);
+      var repordData = new Date();
 
       var getNextMonthReportBody = {
         'app': sysid.INV.app_id.report,
-        'query': 'report_key = "' + sendDate + '" order by 更新日時 asc'
+        'query': 'report_key = "' + REPORT_KEY + '" order by 更新日時 asc'
       };
       kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getNextMonthReportBody)
         .then(function (resp) {
