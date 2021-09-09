8c39af1f7f0f9d6397fc45794ba9098b2a9ecdca
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-09 20:26:59 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -16,8 +16,8 @@
   });
 
   kintone.events.on('app.record.edit.change.invoiceNum', function(event){
-    // if(event.record.invoiceNum.value==='') setFieldShown('invoiceStatus', false);
-    // else setFieldShown('invoiceStatus', true);
+    if(event.record.invoiceNum.value==='') setFieldShown('invoiceStatus', false);
+    else setFieldShown('invoiceStatus', true);
   });
   kintone.events.on(['app.record.create.show', 'app.record.detail.show', 'app.record.edit.show'], function (event) {
     event.record.cSales.disabled = false;
