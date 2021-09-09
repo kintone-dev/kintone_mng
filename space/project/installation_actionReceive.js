4ac988521892d0d91429940cf758266c63462ece
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-09 19:58:30 +0900

    Update installation_actionReceive.js

--- space/project/installation_actionReceive.js
+++ space/project/installation_actionReceive.js
@@ -6,8 +6,8 @@
     event.record.prjNum.value = sessionStorage.getItem('prjNum');
     event.record.bnName.value = sessionStorage.getItem('unknowINST');
     event.record.bName.value = sessionStorage.getItem('unknowINST');
-    event.record.bnName.disabled = true;//sessionStorage.getItem('setShown');
-    event.record.bName.disabled = true;//sessionStorage.getItem('setShown');
+    event.record.bnName.disabled = sessionStorage.getItem('setShown');
+    event.record.bName.disabled = sessionStorage.getItem('setShown');
     alert(sessionStorage.getItem('setShown'));
     // setSpaceShown('btn_newORG','individual',sessionStorage.getItem('btn_newORG_shown'))
     //キャンセルした時の処理
