6ef3a1c7226ed5abb2377724f36d43d2684a9ec7
kintone-mng <kintone_mng@accel-lab.com>
2021-09-13 18:59:01 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -9,24 +9,24 @@
     return event;
   });
 
-  var prjNumValue='';
-  kintone.events.on('app.record.create.change.prjNum', function(event) {
-    prjNumValue=event.record.prjNum.value;
+  var prjNumValue = '';
+  kintone.events.on('app.record.create.change.prjNum', function (event) {
+    prjNumValue = event.record.prjNum.value;
     return event;
   });
 
-  kintone.events.on('app.record.edit.change.invoiceNum', function(event){
-    if(event.record.invoiceNum.value===''||event.record.invoiceNum.value===undefined) setFieldShown('invoiceStatus', false);
+  kintone.events.on('app.record.edit.change.invoiceNum', function (event) {
+    if (event.record.invoiceNum.value === '' || event.record.invoiceNum.value === undefined) setFieldShown('invoiceStatus', false);
     else setFieldShown('invoiceStatus', true);
   });
   kintone.events.on(['app.record.create.show', 'app.record.detail.show', 'app.record.edit.show'], function (event) {
     event.record.cSales.disabled = false;
     setFieldShown('sys_suptitle', true);
-    if(event.record.invoiceNum.value===''||event.record.invoiceNum.value===undefined) setFieldShown('invoiceStatus', false);
+    if (event.record.invoiceNum.value === '' || event.record.invoiceNum.value === undefined) setFieldShown('invoiceStatus', false);
     else setFieldShown('invoiceStatus', true);
     // 新・既存案件表示切り替え
-    function tabSwitch(onSelect){
-      switch(onSelect){
+    function tabSwitch(onSelect) {
+      switch (onSelect) {
         case '#案件情報':
           setFieldShown('prjNum', true);
           setFieldShown('Exist_Project', true);
@@ -61,10 +61,10 @@
           setFieldShown('purchaseOrder', false);
           setFieldShown('prjMemo', false);
           setFieldShown('samePRJ', false);
-          if(event.record.instName.value==undefined){
+          if (event.record.instName.value == undefined) {
             setSpaceShown('btn_newINST', 'individual', 'block');
             setSpaceShown('btn_unknowINST', 'individual', 'block');
-          }else{
+          } else {
             setSpaceShown('btn_newINST', 'individual', 'none');
             setSpaceShown('btn_unknowINST', 'individual', 'none');
           }
@@ -160,24 +160,25 @@
           setFieldShown('expArrivalDate', true);
           break;
       }
-    }tabSwitch('#案件情報');
+    }
+    tabSwitch('#案件情報');
     //タブメニュー作成
-    tabMenu('tab_project', ['案件情報','設置先情報','納品依頼リスト','宛先情報','輸送情報']);
+    tabMenu('tab_project', ['案件情報', '設置先情報', '納品依頼リスト', '宛先情報', '輸送情報']);
     //タブ切り替え表示設定
-    $('.tab_project a').on('click', function(){
-      var idName = $(this).attr('href');//タブ内のリンク名を取得
-      tabSwitch(idName);//tabをクリックした時の表示設定
-      return false;//aタグを無効にする
+    $('.tab_project a').on('click', function () {
+      var idName = $(this).attr('href'); //タブ内のリンク名を取得
+      tabSwitch(idName); //tabをクリックした時の表示設定
+      return false; //aタグを無効にする
     });
-    var newINST=setBtn('btn_newINST','新規設置先');
-    $('#'+newINST.id).on('click', function(){
+    var newINST = setBtn('btn_newINST', '新規設置先');
+    $('#' + newINST.id).on('click', function () {
       // createNewREC(sysid.PM.app_id.installation, ['prjNum', 'btn_newORG_shown'], [prjNumValue, 'none']);
-      createNewREC(sysid.PM.app_id.installation, ['prjNum','unknowINST','setShown'], [prjNumValue,'','']);
+      createNewREC(sysid.PM.app_id.installation, ['prjNum', 'unknowINST', 'setShown'], [prjNumValue, '', '']);
     });
 
-    var unknowINST=setBtn('btn_unknowINST','新規不特定設置先');
-    $('#'+unknowINST.id).on('click', function(){
-      createNewREC(sysid.PM.app_id.installation, ['prjNum','unknowINST','setShown'], [prjNumValue,'不特定設置先','disable']);
+    var unknowINST = setBtn('btn_unknowINST', '新規不特定設置先');
+    $('#' + unknowINST.id).on('click', function () {
+      createNewREC(sysid.PM.app_id.installation, ['prjNum', 'unknowINST', 'setShown'], [prjNumValue, '不特定設置先', 'disable']);
     });
   });
   kintone.events.on(['app.record.index.edit.show', 'app.record.edit.show'], function (event) {
@@ -200,59 +201,59 @@
     }
   });
 
-/*
-  kintone.events.on('app.record.detail.process.proceed', function (event) {
-    var nStatus = event.nextStatus.value;
-    if (nStatus == '納品手配済') {
-      var queryBody = {
-        'app': sysID.DIPM.app.ship,
-        'query': 'prjNum="' + event.record.prjNum.value + '" and ステータス in ("納品情報未確定")',
-        'fields': ['prjNum', '$id', 'ステータス', 'shipType']
-      };
-
-      kintone.api(kintone.api.url('/k/v1/records', true), 'GET', queryBody).then(function (getResp) {
-        //「確認中」の「用途」がある場合、「用途」を更新するBody作成
-        var update_shipType = {
+  /*
+    kintone.events.on('app.record.detail.process.proceed', function (event) {
+      var nStatus = event.nextStatus.value;
+      if (nStatus == '納品手配済') {
+        var queryBody = {
           'app': sysID.DIPM.app.ship,
-          'records': []
-        };
-        //Statusの更新用Body作成
-        var update_Status = {
-          'app': sysID.DIPM.app.ship,
-          'records': []
+          'query': 'prjNum="' + event.record.prjNum.value + '" and ステータス in ("納品情報未確定")',
+          'fields': ['prjNum', '$id', 'ステータス', 'shipType']
         };
 
-        for (var i in getResp.records) {
-          //「確認中」の「用途」がある場合、update_shipTypeのrecordsに追加
-          if (getResp.records[i].shipType.value == '確認中') {
-            update_shipType.records.push({
-              'id': getResp.records[i].$id.value,
-              'record': {
-                'shipType': {
-                  'value': event.record.shipType.value
+        kintone.api(kintone.api.url('/k/v1/records', true), 'GET', queryBody).then(function (getResp) {
+          //「確認中」の「用途」がある場合、「用途」を更新するBody作成
+          var update_shipType = {
+            'app': sysID.DIPM.app.ship,
+            'records': []
+          };
+          //Statusの更新用Body作成
+          var update_Status = {
+            'app': sysID.DIPM.app.ship,
+            'records': []
+          };
+
+          for (var i in getResp.records) {
+            //「確認中」の「用途」がある場合、update_shipTypeのrecordsに追加
+            if (getResp.records[i].shipType.value == '確認中') {
+              update_shipType.records.push({
+                'id': getResp.records[i].$id.value,
+                'record': {
+                  'shipType': {
+                    'value': event.record.shipType.value
+                  }
                 }
-              }
+              });
+            }
+            update_Status.records.push({
+              'id': getResp.records[i].$id.value,
+              'action': '処理開始',
+              //'assignee': 'kintone_mng@accel-lab.com'
             });
           }
-          update_Status.records.push({
-            'id': getResp.records[i].$id.value,
-            'action': '処理開始',
-            //'assignee': 'kintone_mng@accel-lab.com'
-          });
-        }
-        if (update_shipType.records.length > 0) kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', update_shipType); //.then(function(resp){console.log(resp)}).catch(function(error){console.log(error)});
-        kintone.api(kintone.api.url('/k/v1/records/status', true), 'PUT', update_Status); //.then(function(resp){console.log(resp)}).catch(function(error){console.log(error)});
+          if (update_shipType.records.length > 0) kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', update_shipType); //.then(function(resp){console.log(resp)}).catch(function(error){console.log(error)});
+          kintone.api(kintone.api.url('/k/v1/records/status', true), 'PUT', update_Status); //.then(function(resp){console.log(resp)}).catch(function(error){console.log(error)});
 
-      }).catch(function (error) {
-        console.log(error);
-        console.log(error.message);
-      });
+        }).catch(function (error) {
+          console.log(error);
+          console.log(error.message);
+        });
 
-      var putBody = {
+        var putBody = {
 
-      };
-    }
-    return event;
-  });
-*/
+        };
+      }
+      return event;
+    });
+  */
 })();
