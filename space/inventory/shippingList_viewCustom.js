31a8e8e47c2ef12e133a016b4064fa8f9468e048
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-14 14:59:04 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -191,10 +191,11 @@
   // カーテンレールが選択された場合、シリアル番号欄にデータを記入
   kintone.events.on(['app.record.edit.change.mCode','app.record.create.change.mCode'], function(event) {
     for (var i in event.record.deviceList.value){
-      if(event.record.deviceList.value[i].value.mCode.value=='TRT-DY'){
-        // if(event.record.deviceList.value[i].value.shipRemarks.value===undefined){
-          event.record.deviceList.value[i].value.shipRemarks.value='カーテンレール全長(mm)：\n開き勝手：(S)片開き/(W)両開き\n取り付け方法：天井/壁付S/壁付W';
-        // }
+      var mCodeValue=event.record.deviceList.value[i].value.mCode.value;
+      if(mCodeValue=='TRT-DY'){
+        event.record.deviceList.value[i].value.shipRemarks.value='WFP\nカーテンレール全長(mm)：\n開き勝手：(S)片開き/(W)両開き\n取り付け方法：天井/壁付S/壁付W';
+      }else if(mCodeValue.match(/pkg_/)){
+        event.record.deviceList.value[i].value.shipRemarks.value='WFP';
       }
     }
     return event;
