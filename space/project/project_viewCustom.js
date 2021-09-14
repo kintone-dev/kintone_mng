c2e2c98cfee68265cf2d9cdf9f75e78bcee4bf22
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-09-14 19:58:34 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -395,14 +395,69 @@ var setEasySearch=function(sID,sPlaceholder) {
 
 	kintone.app.getHeaderMenuSpaceElement().appendChild(eSearchForm);
 }
-
+//検索したいフィールドの設定値
+//ふぃーるどフィールドコードは一対一
+const FIELD_CODE = 'invoiceNum';
+const FIELD_CODE2 = 'prjNum';
+//andかorを小文字で入れる、今回はor
+const AND_OR = "or";
   kintone.events.on('app.record.index.show', function(event){
     setEasySearch('eSearch','総合検索');
     $('#btn_eSearch').on('click', function(){
       // var testC=document.s_eSearch.value;
       var testC=document.eSearch.s_eSearch.value;
       console.log(testC);
-    })
+
+      var result = {};
+      //クエリから、URL固定部分(?query=)を無視して取り出す
+      var query = window.location.search.substring(7); 
+      //フィールドコード名と検索キーワードに分割する
+      for(var i = 0;i < query.length;i++){
+        var element = query[i].split('like');
+        var param_field_code = encodeURIComponent(element[0]);
+        var param_search_word = encodeURIComponent(element[1]);
+        //空白スペースを取り除いて、配列に格納
+        result[param_field_code.replace(/^\s+|\s+$/g, "")] = param_search_word.replace(/^[\s|\"]+|[\s|\"]+$/g, "");
+      }
+      //検索キーワード
+      var search_word = document.createElement('input');
+      search_word.type = 'text';
+      //検索ボタン
+      var search_button = document.createElement('input');
+      search_button.type = 'submit';
+      search_button.value = 'search';
+      search_button.onclick = function () {
+        keyword_search();
+      };
+      //キーワード検索の関数
+      function keyword_search(){
+        var keyword = search_word.value;
+        // var str_query = '?query='+ FIELD_CODE +' like "' + keyword;
+        // ここがクエリ
+        // var str_query = '?query='+ FIELD_CODE +' like "' + keyword + '" ' + AND_OR +' '+ FIELD_CODE2 +' like "' + keyword + '"' + AND_OR +' '+ FIELD_CODE3 +' like "' + keyword + '"';
+        var str_query = '?query='+ FIELD_CODE +' like "' + keyword + '" ' + AND_OR +' '+ FIELD_CODE2 +' like "' + keyword + '"';
+        
+        if(keyword == ""){
+          str_query = "";
+        }else if(keyword != ""){
+          // str_query = '?query='+ FIELD_CODE +' like "' + keyword + '"'; //コメントアウト
+        }
+        // 検索結果のURLへ
+        document.location = location.origin + location.pathname + str_query
+      }
+      // 重複を避けるため要素をあらかじめクリアしておく
+      var node_space = kintone.app.getHeaderMenuSpaceElement()
+      for (var i =node_space.childNodes.length-1; i>=0; i--) {
+        node_space.removeChild(node_space.childNodes[i]);
+      }
+      var label = document.createElement('label');
+      label.appendChild(document.createTextNode('レコード内検索'));
+      label.appendChild(document.createTextNode(' ')); 
+      label.appendChild(search_word);
+      label.appendChild(document.createTextNode(' ')); 
+      label.appendChild(search_button); 
+      kintone.app.getHeaderMenuSpaceElement().appendChild(label);
+    });
     // setEasySearch({
     //   id:'eSearch',
     //   placeholder:'総合検索',
@@ -421,6 +476,10 @@ var setEasySearch=function(sID,sPlaceholder) {
     //     {tar_fCode:'prjNum',tar_fValue:''}
     //   ]
     // };
+    //GET引数に格納された直前の検索キーワードを取得して再表示
+
+  　
+    return event;
   });
 /*
   const tar_fCode=['invoiceNum','prjNum'];
