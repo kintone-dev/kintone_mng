94b9a905eddf4db67633682e04b52a5a90f8b0d8
kintone-mng <kintone_mng@accel-lab.com>
2021-09-14 12:01:17 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -101,11 +101,11 @@
           PAGE_RECORD[fcode].disabled = true;
         }
       });
-      for(var i in record['SUBTABLE'].value){
-        record['SUBTABLE']['value'][i]['value']['mNickname']['disabled'] = true;
-        record['SUBTABLE']['value'][i]['value']['shipNum']['disabled'] = true;
-        record['SUBTABLE']['value'][i]['value']['subBtn']['disabled'] = true;
-        record['SUBTABLE']['value'][i]['value']['shipRemarks']['disabled'] = true;
+      for(var i in PAGE_RECORD['SUBTABLE'].value){
+        PAGE_RECORD['SUBTABLE']['value'][i]['value']['mNickname']['disabled'] = true;
+        PAGE_RECORD['SUBTABLE']['value'][i]['value']['shipNum']['disabled'] = true;
+        PAGE_RECORD['SUBTABLE']['value'][i]['value']['subBtn']['disabled'] = true;
+        PAGE_RECORD['SUBTABLE']['value'][i]['value']['shipRemarks']['disabled'] = true;
     }
       PAGE_RECORD.invoiceDate.disabled = false;
       PAGE_RECORD.invoiceNum.disabled = false;
