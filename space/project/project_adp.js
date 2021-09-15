1f22f40d3897d7df51ac23c3dca3f9ad4e058cec
kintone-mng <kintone_mng@accel-lab.com>
2021-09-15 16:53:47 +0900

    Update project_adp.js

--- space/project/project_adp.js
+++ space/project/project_adp.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   kintone.events.on('app.record.detail.process.proceed', function (event) {
@@ -7,63 +7,63 @@
     if (nStatus === '納品準備中') {
       var postShipData = [];
       var postShipBody = {
-        'aboutDelivery':{
-          'value':PAGE_RECORD.aboutDelivery.value
+        'aboutDelivery': {
+          'value': PAGE_RECORD.aboutDelivery.value
         },
-        'tarDate':{
-          'value':PAGE_RECORD.tarDate.value
+        'tarDate': {
+          'value': PAGE_RECORD.tarDate.value
         },
-        'dstSelection':{
-          'value':PAGE_RECORD.dstSelection.value
+        'dstSelection': {
+          'value': PAGE_RECORD.dstSelection.value
         },
-        'Contractor':{
-          'value':PAGE_RECORD.Contractor.value
+        'Contractor': {
+          'value': PAGE_RECORD.Contractor.value
         },
-        'instName':{
-          'value':PAGE_RECORD.instName.value
+        'instName': {
+          'value': PAGE_RECORD.instName.value
         },
-        'receiver':{
-          'value':PAGE_RECORD.receiver.value
+        'receiver': {
+          'value': PAGE_RECORD.receiver.value
         },
-        'phoneNum':{
-          'value':PAGE_RECORD.phoneNum.value
+        'phoneNum': {
+          'value': PAGE_RECORD.phoneNum.value
         },
-        'zipcode':{
-          'value':PAGE_RECORD.zipcode.value
+        'zipcode': {
+          'value': PAGE_RECORD.zipcode.value
         },
-        'prefectures':{
-          'value':PAGE_RECORD.prefectures.value
+        'prefectures': {
+          'value': PAGE_RECORD.prefectures.value
         },
-        'city':{
-          'value':PAGE_RECORD.city.value
+        'city': {
+          'value': PAGE_RECORD.city.value
         },
-        'address':{
-          'value':PAGE_RECORD.address.value
+        'address': {
+          'value': PAGE_RECORD.address.value
         },
-        'buildingName':{
-          'value':PAGE_RECORD.buildingName.value
+        'buildingName': {
+          'value': PAGE_RECORD.buildingName.value
         },
-        'corpName':{
-          'value':PAGE_RECORD.corpName.value
+        'corpName': {
+          'value': PAGE_RECORD.corpName.value
         },
-        'sys_instAddress':{
-          'value':PAGE_RECORD.sys_instAddress.value
+        'sys_instAddress': {
+          'value': PAGE_RECORD.sys_instAddress.value
         },
-        'sys_unitAddress':{
-          'value':PAGE_RECORD.sys_unitAddress.value
+        'sys_unitAddress': {
+          'value': PAGE_RECORD.sys_unitAddress.value
         },
-        'deviceList':{
-          'value':[]
+        'deviceList': {
+          'value': []
         },
       }
-      for(var pdv in PAGE_RECORD.deviceList.value){
+      for (var pdv in PAGE_RECORD.deviceList.value) {
         var devListBody = {
-          'value':{
-            'mName':{
-              'value':PAGE_RECORD.deviceList.value[pdv].value.mNickname.value
+          'value': {
+            'mName': {
+              'value': PAGE_RECORD.deviceList.value[pdv].value.mNickname.value
             },
-            'shipNum':{
-              'value':PAGE_RECORD.deviceList.value[pdv].value.shipNum.value
+            'shipNum': {
+              'value': PAGE_RECORD.deviceList.value[pdv].value.shipNum.value
             }
           }
         }
@@ -84,17 +84,18 @@
       'query': 'sys_invoiceDate = "' + PAGE_RECORD.sys_invoiceDate.value + '" order by 更新日時 asc'
     };
     return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getReportBody)
-    .then(function (resp) {
-      // var eRecord=kintone.app.record.get();
-      for(var i in resp.records[0].EoMcheck.value ){
-        if(resp.records[0].EoMcheck.value[i] == '締切'){
-          alert('対応した日付のレポートは締切済みです。');
-          event.record.invoiceYears.value = '2025';
-          event.error = '対応した日付のレポートは締切済みです。';
-          return event;
+      .then(function (resp) {
+        if (resp.records != 0) {
+          for (var i in resp.records[0].EoMcheck.value) {
+            if (resp.records[0].EoMcheck.value[i] == '締切') {
+              alert('対応した日付のレポートは締切済みです。');
+              event.record.invoiceYears.value = '2025';
+              event.error = '対応した日付のレポートは締切済みです。';
+              return event;
+            }
+          }
         }
-      }
-    });
+      });
 
   });
 
