e154ca2b7c272ca86f00c37676974e8f3c2e39ac
kintone-mng <kintone_mng@accel-lab.com>
2021-09-16 17:05:38 +0900

    Update project_adp.js

--- space/project/project_adp.js
+++ space/project/project_adp.js
@@ -236,7 +236,7 @@
 
     var nowDate = new Date();
     var nowDateFormat = String(nowDate.getFullYear()) + String(("0"+(nowDate.getMonth() + 1)).slice(-2));
-    if(parseInt(nowDateFormat) < parseInt(PAGE_RECORD.sys_invoiceDate.value)){
+    if(parseInt(nowDateFormat) > parseInt(PAGE_RECORD.sys_invoiceDate.value)){
       event.error = '請求月が間違っています。';
       return event;
     }
