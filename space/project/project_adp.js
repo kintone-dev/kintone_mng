d1beb8799de08cf1cbde8d4f5b78f007635bc730
kintone-mng <kintone_mng@accel-lab.com>
2021-09-16 16:23:31 +0900

    Update project_adp.js

--- space/project/project_adp.js
+++ space/project/project_adp.js
@@ -92,8 +92,8 @@
                 return postRecords(sysid.INV.app_id.shipment, postShipData);
 
               } else if (nStatus === '完了') {
+                console.log(PAGE_RECORD.salesType.value);
                 if (PAGE_RECORD.salesType.value == '販売' || PAGE_RECORD.salesType.value == 'サブスク') {
-                  console.log('ok');
                   //積送在庫処理
                   var stockData = []
                   //納品リストからmCodeと納品数を取得
