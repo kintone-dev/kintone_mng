83a22853a8ec42b3f370b3ff6672e41462d7b0d9
kintone-mng <kintone_mng@accel-lab.com>
2021-09-17 10:35:15 +0900

    Update report_viewCustom.js

--- space/inventory/report_viewCustom.js
+++ space/inventory/report_viewCustom.js
@@ -97,6 +97,7 @@
     //差引数量マイナスのものを赤背景に
     for(var i in deductionData){
       if(parseInt(deductionData[i].deductionNum) < 0){
+        console.log('.'+ tableClass + ' tr:nth-child('+ deductionData[i].rowNum +')');
         $('.'+ tableClass + ' tr:nth-child('+ deductionData[i].rowNum +')').css({
           'bacground-color':'red'
         });
