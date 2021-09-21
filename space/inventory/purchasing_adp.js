c65369d39e65c143baddd6d45b37f684b8c71b4a
kintone-mng <kintone_mng@accel-lab.com>
2021-09-21 11:23:21 +0900

    Update purchasing_adp.js

--- space/inventory/purchasing_adp.js
+++ space/inventory/purchasing_adp.js
@@ -1,5 +1,40 @@
 (function () {
   'use strict';
 
+  kintone.events.on('app.record.detail.process.proceed', function (event) {
+    const PAGE_RECORD = event.record;
+    var nStatus = event.nextStatus.value;
 
+    if(nStatus==="仕入完了"){
+      putDevice(PAGE_RECORD);
+    }
+  });
+
+  const putDevice = function (pageRecod) {
+    var arrivalList = pageRecod.arrivalList.value;
+    var deviceQuery = [];
+    for (var i in arrivalList) {
+      deviceQuery.push('"' + arrivalList[i].value.mCode.value + '"');
+    }
+    var getDeviceBody = {
+      'app': sysid.INV.app_id.device,
+      'query': 'mCode in (' + deviceQuery.join() + ') order by 更新日時 asc'
+    };
+    kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getUnitBody)
+    .then(function (resp) {
+      var deviceRecords = resp.records;
+      console.log(deviceRecords);
+      var putDevData = [];
+
+      // for(var i in arrivalList){
+      //   for(var j in deviceRecords){
+      //     if(arrivalList[i].value.mCode.value == deviceRecords.){
+
+      //     }
+      //   }
+      // }
+
+    });
+
+  }
 })();
