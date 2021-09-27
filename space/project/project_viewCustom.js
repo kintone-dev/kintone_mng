1b7d24bbaec7fafc19d3fe1edfe29a3f3f1885bb
kintone-mng <kintone_mng@accel-lab.com>
2021-09-27 17:30:40 +0900

    Update project_viewCustom.js

--- space/project/project_viewCustom.js
+++ space/project/project_viewCustom.js
@@ -260,8 +260,8 @@
 
   kintone.events.on(['app.record.edit.change.sys_instAddress', 'app.record.create.change.sys_instAddress'], function (event) {
     if(event.record.instName.value=='' || event.record.instName.value==undefined){
-      setSpaceShown('btn_newINST','individual','table-block');
-      setSpaceShown('btn_unknowINST','individual','table-block');
+      setSpaceShown('btn_newINST','individual','inline-block');
+      setSpaceShown('btn_unknowINST','individual','inline-block');
       console.log('no');
     }
     else{
