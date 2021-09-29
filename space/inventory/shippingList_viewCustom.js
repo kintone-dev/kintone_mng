55eb1895960326fd193910eb2216c989441d5978
kintone-mng <kintone_mng@accel-lab.com>
2021-09-29 16:57:38 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -247,6 +247,7 @@
 
   // 計算ボタン
   kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
+    console.log(stockCtrl(event));
     setBtn('calBtn', '計算');
     $('#calBtn').on('click', function () {
       var eRecord = kintone.app.record.get();
