57fd462f4beefa54d0922e3362904884f7625fd9
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-04 10:33:42 +0900

    Update developSetting.js

--- general/js/developSetting.js
+++ general/js/developSetting.js
@@ -13,13 +13,18 @@
   ];
   // indexページでの新規、編集、複製ボタン非表示
   kintone.events.on(events_ced, function (event) {
-    var ignoreUser = ['sysdev','kintone_mng@accel-lab.com']
+    //編集を表示するユーザー
+    var ignoreUser = ['sysdev','kintone_mng@accel-lab.com'];
+    //編集を表示しないページ
+    var deletePage = [sysid.INV.app_id.report];
     if(!ignoreUser.includes(kintone.getLoginUser().code)){
-      $('.gaia-argoui-app-menu-add').remove();
-      $('.recordlist-edit-gaia').remove();
-      $('.recordlist-remove-gaia').remove();
-      $('.gaia-argoui-app-menu-edit').remove();
-      $('.gaia-argoui-app-menu-copy').remove();
+      if(deletePage.includes(kintone.app.getId())){
+        $('.gaia-argoui-app-menu-add').remove();
+        $('.recordlist-edit-gaia').remove();
+        $('.recordlist-remove-gaia').remove();
+        $('.gaia-argoui-app-menu-edit').remove();
+        $('.gaia-argoui-app-menu-copy').remove();
+      }
     }
     return event;
   });
