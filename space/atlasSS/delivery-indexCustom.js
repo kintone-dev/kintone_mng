18e07a8805924457e511b5ae63a42bd41e1f2316
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-04 12:19:16 +0900

    Update delivery-indexCustom.js

--- space/atlasSS/delivery-indexCustom.js
+++ space/atlasSS/delivery-indexCustom.js
@@ -27,10 +27,11 @@
           async: false
         })
       }
-      var nowDate = getNowDate().done(function (data, status, xhr) {
+      var nowDate = await getNowDate().done(function (data, status, xhr) {
         //請求月が今より過去の場合
         var serverDate = new Date(xhr.getResponseHeader('Date')); //サーバー時刻を代入
         var nowDateFormat = String(serverDate.getFullYear()) + String(("0" + (serverDate.getMonth() + 1)).slice(-2));
+        console.log(nowDateFormat);
         return nowDateFormat;
       });
       console.log(nowDate);
