3e5a14e78062d849a7521df0eaf388af9f6e4286
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-08 17:45:33 +0900

    Update loader.js

--- general/js/loader.js
+++ general/js/loader.js
@@ -8,6 +8,22 @@ function startLoad(msg) {
   }
 }
 
+function startLoad2(msgType){
+  var logingFrame=document.createElement('div');
+  var dispMsg=document.createElement('div');
+  dispMsg.classList.add('loadingMsg');
+  switch(msgType){
+    default:
+      var textLine1=document.createElement('p');
+      textLine1.innerText='処理中';
+      dispMsg.appendChild(textLine1);
+  }
+  logingFrame.appendChild(dispMsg);
+  if ($("#loading").length == 0) {
+    $("body").appendChild(logingFrame);
+  }
+}
+
 function endLoad(){
   $("#loading").remove();
 }
