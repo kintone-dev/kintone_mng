66fb05fe22e274bad0cfe8bc5e4bf25cb3374edc
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-08 12:56:14 +0900

    Update device_viewCustom.js

--- space/inventory/device_viewCustom.js
+++ space/inventory/device_viewCustom.js
@@ -204,8 +204,6 @@
   kintone.events.on(['app.record.create.change.mType'], function(event){
     var mcode=event.record.mCode;
     var mtype=event.record.mType;
-    console.log(mcode.value);
-    console.log(mtype.value);
     if(mtype.value=='パッケージ品'){
       if(mcode.value==undefined){
         mcode.value='pkg_';
@@ -229,10 +227,8 @@
   // 取扱区分における品目コード制御
   kintone.events.on(['app.record.create.change.mClassification'], function(event){
     var mcode=event.record.mCode;
-    var mtype=event.record.mType;
-    console.log(mcode.value);
-    console.log(mtype.value);
-    if(mtype.value=='非在庫'){
+    var mclassification=event.record.mClassification;
+    if(mclassification.value=='非在庫'){
       if(mcode.value==undefined){
         mcode.value='ns_';
       }
@@ -249,5 +245,4 @@
     console.log('after: '+mcode.value);
     return event;
   });
-
 })();
