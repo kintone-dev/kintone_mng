63935e9f7c2f79a9b619f06ec52d17f1b342067c
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-08 18:03:27 +0900

    Update loader.js

--- general/js/loader.js
+++ general/js/loader.js
@@ -8,19 +8,3 @@ function startLoad(msg) {
   }
 }
 
-function startLoad2(msgType){
-  var logingFrame=document.createElement('div');
-  var dispMsg=document.createElement('div');
-  dispMsg.classList.add('loadingMsg');
-  var textLine1=document.createElement('p');
-  textLine1.innerText=lodingMSG(msgType);
-  dispMsg.appendChild(textLine1);
-  logingFrame.appendChild(dispMsg);
-  if ($("#loading").length == 0) {
-    $("body").appendChild(logingFrame);
-  }
-}
-
-function endLoad(){
-  $("#loading").remove();
-}
