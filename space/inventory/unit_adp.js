6c3fd6331cdf7eb27f52a52d6a4356a6868a37bc
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-12 12:37:28 +0900

    Update unit_adp.js

--- space/inventory/unit_adp.js
+++ space/inventory/unit_adp.js
@@ -1,8 +1,5 @@
 (function () {
   'use strict';
-  //商品情報取得＆繰り返し利用
-  var getDEVdata = api_getRecords(sysid.INV.app_id.device);
-
   //新規拠点作成時アクション
   kintone.events.on('app.record.create.show', async function (event) {
     startLoad();
@@ -48,80 +45,99 @@
     }
     kintone.app.record.set(eRecord);
     endLoad();
-
     return event;
   });
 
   //新規保存時アクション
   kintone.events.on('app.record.create.submit.success', async function (event) {
     startLoad();
-    //品目情報を拠点リストに転送
-    getDEVdata.then(function (resp) {
-      var tarRecords = resp.records;
-      //商品管理アプリの拠点リストに上書きするデータ作成
-      var NewPrdInfo = {
-        'app': sysid.INV.app_id.device,
-        'records': []
+    var getDevBody = {
+      'app': sysid.INV.app_id.device,
+      'query': null
+    };
+    var devRecord = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getDevBody)
+      .then(function (resp) {
+        return resp;
+      }).catch(function (error) {
+        console.log(error);
+        return error;
+      });
+    var tarRecords = devRecord.records;
+    //商品管理アプリの拠点リストに上書きするデータ作成
+    var NewPrdInfo = {
+      'app': sysid.INV.app_id.device,
+      'records': []
+    };
+    for (var i in tarRecords) {
+      var records_set = {
+        'id': tarRecords[i].$id.value,
+        'record': {
+          'uStockList': tarRecords[i].uStockList
+        }
       };
-      for (var i in tarRecords) {
-        var records_set = {
-          'id': tarRecords[i].$id.value,
-          'record': {
-            'uStockList': tarRecords[i].uStockList
-          }
-        };
-        var addRowData = {
-          'value': {
-            'uCode': event.record.uCode,
-            'uName': event.record.uName
-          }
-        };
-        records_set.record.uStockList.value.push(addRowData);
-        NewPrdInfo.records.push(records_set);
-      }
-      return kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', NewPrdInfo);
-    }).then(function (resp) {
-      //転送成功
-      console.log('商品管理に新規拠点を追加');
-    });
+      var addRowData = {
+        'value': {
+          'uCode': event.record.uCode,
+          'uName': event.record.uName
+        }
+      };
+      records_set.record.uStockList.value.push(addRowData);
+      NewPrdInfo.records.push(records_set);
+    }
+    await kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', NewPrdInfo)
+      .then(function (resp) {
+        //転送成功
+        console.log('商品管理に新規拠点を追加');
+      });
     endLoad();
+    return event;
   });
 
   //編集保存時アクション
   kintone.events.on('app.record.edit.submit.success', async function (event) {
     startLoad();
-    //品目情報を拠点リストに転送
-    getDEVdata.then(function (resp) {
-      var tarRecords = resp.records;
-      //商品管理アプリの拠点リストに上書きするデータ作成
-      var NewPrdInfo = {
-        'app': sysid.INV.app_id.device,
-        'records': []
+    var getDevBody = {
+      'app': sysid.INV.app_id.device,
+      'query': null
+    };
+    var devRecord = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', getDevBody)
+      .then(function (resp) {
+        return resp;
+      }).catch(function (error) {
+        console.log(error);
+        return error;
+      });
+    var tarRecords = devRecord.records;
+    //商品管理アプリの拠点リストに上書きするデータ作成
+    var NewPrdInfo = {
+      'app': sysid.INV.app_id.device,
+      'records': []
+    };
+    //sud: set unit data
+    for (var sud in tarRecords) {
+      var records_set = {
+        'id': tarRecords[sud].$id.value,
+        'record': {
+          'uStockList': tarRecords[sud].uStockList
+        }
       };
-      //sud: set unit data
-      for (var sud in tarRecords) {
-        var records_set = {
-          'id': tarRecords[sud].$id.value,
-          'record': {
-            'uStockList': tarRecords[sud].uStockList
-          }
-        };
-        NewPrdInfo.records.push(records_set);
-      }
-      //編集した拠点名を反映
-      for (var i in NewPrdInfo.records) {
-        for (var j in NewPrdInfo.records[i].record.uStockList.value) {
-          if (NewPrdInfo.records[i].record.uStockList.value[j].value.uCode.value == event.record.uCode.value) {
-            NewPrdInfo.records[i].record.uStockList.value[j].value.uName.value = event.record.uName.value;
-          }
+      NewPrdInfo.records.push(records_set);
+    }
+    //編集した拠点名を反映
+    for (var i in NewPrdInfo.records) {
+      for (var j in NewPrdInfo.records[i].record.uStockList.value) {
+        if (NewPrdInfo.records[i].record.uStockList.value[j].value.uCode.value == event.record.uCode.value) {
+          NewPrdInfo.records[i].record.uStockList.value[j].value.uName.value = event.record.uName.value;
         }
       }
-      return kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', NewPrdInfo);
-    }).then(function (resp) {
-      //転送成功
-      console.log('商品管理の拠点リストを編集');
-    });
+    }
+    await kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', NewPrdInfo)
+      .then(function (resp) {
+        //転送成功
+        console.log('商品管理の拠点リストを編集');
+      });
     endLoad();
+    return event;
   });
 
 })();
