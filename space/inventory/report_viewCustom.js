b0a2262733b96ef47f4a3487189097cb56084684
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-12 15:46:22 +0900

    Update report_viewCustom.js

--- space/inventory/report_viewCustom.js
+++ space/inventory/report_viewCustom.js
@@ -106,10 +106,9 @@
       table = sortItemTable(table, 'sys_code', true);
       kintone.app.record.set(eRecord);
       for (var i in eRecord.record.inventoryList.value) {
-        console.log(i);
         eRecord.record.inventoryList.value[i].value.mCode.lookup = true;
       }
-      // endLoad();
+      endLoad();
     });
 
     $('#locationSortBtn').on('click', async function () {
