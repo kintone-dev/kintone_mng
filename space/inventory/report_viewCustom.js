8c4e80849775b9063d3e39440b8ed2bbb6782c33
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-12 11:07:27 +0900

    Update report_viewCustom.js

--- space/inventory/report_viewCustom.js
+++ space/inventory/report_viewCustom.js
@@ -70,11 +70,22 @@
           break;
       }
     }
-    tabSwitch('#概要');
+    //tab初期表示設定
+    if (sessionStorage.getItem('tabSelect')) {
+      $('.tab_ship li').removeClass("active"); //li要素のCSS設定を削除
+      tabSwitch(sessionStorage.getItem('tabSelect'));
+      $('.tab_ship li:nth-child(' + (parseInt(sessionStorage.getItem('actSelect')) + 1) + ')').addClass('active');
+    } else {
+      tabSwitch('#概要');
+    }
+
     tabMenu('tab_report', ['概要', '在庫リスト', '製品別在庫残数']); //タブメニュー作成
     $('.tabMenu a').on('click', function () { //タブメニュークリック時アクション
       var idName = $(this).attr('href'); //タブ内のリンク名を取得
       tabSwitch(idName); //tabをクリックした時の表示設定
+      var actIndex = $('.tab_ship li.active').index();
+      sessionStorage.setItem('tabSelect', idName);
+      sessionStorage.setItem('actSelect', actIndex);
       return false;
     });
     return event;
