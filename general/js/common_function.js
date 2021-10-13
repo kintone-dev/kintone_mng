b0f2617b19708b267869d418cf9abbcc11c616c8
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-13 15:22:06 +0900

    Update common_function.js

--- general/js/common_function.js
+++ general/js/common_function.js
@@ -1958,8 +1958,8 @@ var mWindow = function () {
 function krtSetting() {
 	var mw = mWindow();
 	mw.contents.innerHTML = '<div class="krtInput"><label>カーテンレール全長(mm)：<input type="text" name="length"></label></div>' +
-		'<div class="krtInput">開き勝手：<label class="radioLabel">(S)片開き<input type="radio" value="(S)片開き" name="openType"></label><label class="radioLabel">(W)両開き<input type="radio" value="(W)両開き" name="openType"></label></div>' +
-		'<div class="krtInput">取り付け方法：<label class="radioLabel">天井<input type="radio" value="天井" name="methodType"></label><label class="radioLabel">壁付S<input type="radio" value="壁付S" name="methodType"></label><label class="radioLabel">壁付W<input type="radio" value="壁付W" name="methodType"></label></div>'+
+		'<div class="krtInput">開き勝手：<label class="radioLabel">(S)片開き<input type="radio" value="(S)片開き" name="openType" checked></label><label class="radioLabel">(W)両開き<input type="radio" value="(W)両開き" name="openType"></label></div>' +
+		'<div class="krtInput">取り付け方法：<label class="radioLabel">天井<input type="radio" value="天井" name="methodType" checked></label><label class="radioLabel">壁付S<input type="radio" value="壁付S" name="methodType"></label><label class="radioLabel">壁付W<input type="radio" value="壁付W" name="methodType"></label></div>'+
 		'<button id="krtSetBtn">登録</button>';
 
 	$('#mwFrame').fadeIn();
