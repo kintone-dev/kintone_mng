486b4461323baffd3dc7d038952ecd50fefcdbc2
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-14 12:07:52 +0900

    Update shippingList_viewCustom.js

--- space/inventory/shippingList_viewCustom.js
+++ space/inventory/shippingList_viewCustom.js
@@ -7,6 +7,21 @@
 
   kintone.events.on(['app.record.create.change.shipType', 'app.record.edit.change.shipType'], function (event) {
     disableSet(event);
+    if (event.record.dstSelection.value == '担当手渡し') {
+      setFieldShown('zipcode', false);
+      setFieldShown('prefectures', false);
+      setFieldShown('city', false);
+      setFieldShown('address', false);
+      setFieldShown('buildingName', false);
+      setFieldShown('corpName', false);
+    } else {
+      setFieldShown('zipcode', true);
+      setFieldShown('prefectures', true);
+      setFieldShown('city', true);
+      setFieldShown('address', true);
+      setFieldShown('buildingName', true);
+      setFieldShown('corpName', true);
+    }
     return event;
   });
 
@@ -381,16 +396,8 @@
     } else if (selection == '担当手渡し') {
       setFieldShown('Contractor', false);
       setFieldShown('instName', false);
-
       event.record.receiver.disabled = false;
       event.record.phoneNum.disabled = false;
-      setFieldShown('zipcode', false);
-      setFieldShown('prefectures', false);
-      setFieldShown('city', false);
-      setFieldShown('address', false);
-      setFieldShown('buildingName', false);
-      setFieldShown('corpName', false);
-
       event.record.zipcode.value = '';
       event.record.prefectures.value = '';
       event.record.city.value = '';
