fda919d652a5d8011ec69b42fbeaef9d25f92dc9
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-14 19:41:19 +0900

    Update delivery-viewCustom.js

--- space/atlasSS/delivery-viewCustom.js
+++ space/atlasSS/delivery-viewCustom.js
@@ -80,7 +80,7 @@
       var shipIName = event.record.instName.value;
       var shipShipment = event.record.shipment.value;
 
-      for (var i in shipTable) {
+      for(let i in shipTable) {
         var ship_member_id = shipTable[i].value.member_id.value;
         var ship_shipnum = shipTable[i].value.shipNum.value;
         var ship_sn = shipTable[i].value.sNum.value;
