7ec3922aa69b6ca2493b0d9025d3eff0537fc7b5
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-14 17:06:21 +0900

    Update report_adp.js

--- space/inventory/report_adp.js
+++ space/inventory/report_adp.js
@@ -145,7 +145,7 @@
        * ASS在庫残数処理
        */
       for (let i in event.record.AssStockList.value) {
-        var reportDate_start = new Date(event.record.invoiceYears.value, event.record.invoiceMonth.value, 1);
+        var reportDate_start = new Date(event.record.invoiceYears.value, event.record.invoiceMonth.value);
         var reportDate_end = new Date(event.record.invoiceYears.value, parseInt(event.record.invoiceMonth.value) + 1);
         reportDate_start = reportDate_start.toISOString();
         reportDate_end = reportDate_end.toISOString();
