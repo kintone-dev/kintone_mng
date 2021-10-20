71cde7a899a096069a6830a82b2345f1a5f4793b
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-20 17:42:36 +0900

    Update common_function.js

--- general/js/common_function.js
+++ general/js/common_function.js
@@ -835,6 +835,8 @@ async function stockCtrl(event, appId) {
 	};
 	await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', putDeviceBody)
 		.then(function (resp) {
+			console.log('商品在庫数変更');
+			console.log(resp);
 			return resp;
 		}).catch(function (error) {
 			console.log(error);
@@ -846,6 +848,8 @@ async function stockCtrl(event, appId) {
 	};
 	await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', putUnitBody)
 		.then(function (resp) {
+			console.log('拠点在庫数変更');
+			console.log(resp);
 			return resp;
 		}).catch(function (error) {
 			console.log(error);
