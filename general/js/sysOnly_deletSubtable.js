e432dca022dbfe015e83045a955e792fec8cbc13
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-20 18:22:03 +0900

    Update sysOnly_deletSubtable.js

--- general/js/sysOnly_deletSubtable.js
+++ general/js/sysOnly_deletSubtable.js
@@ -1,6 +1,6 @@
 function deletSB(subTable, fCode){
   kintone.events.on('app.record.index.record', function(event){
-    console.lop(event);
+    console.log(event);
   });
   // let subTableValue=kintone.app.record.get().record[subTable];
   // for(let i in subTableValue){
