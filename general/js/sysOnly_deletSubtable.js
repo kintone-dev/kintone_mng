06b7f558558475adb4f572ebf4cfce8f76b542ad
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-10-21 10:05:13 +0900

    Update sysOnly_deletSubtable.js

--- general/js/sysOnly_deletSubtable.js
+++ general/js/sysOnly_deletSubtable.js
@@ -1,27 +1,30 @@
-function deletSB(subTable, fCode){
-  kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', {app:kintone.app.getId()}).then(function(resp){
-    let recordList=resp.records;
-    let body={
-      app:kintone.app.getId(),
-      records:[]
-    }
-    for(let i in recordList){
-      let subTableValue=recordList[i][subTable].value;
-      for (let y in subTableValue){
-        subTableValue[y].value[fCode].value='';
+(function () {
+  'use strict';
+  kintone.events.on('app.record.index.show', function(event){
+    kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', {app:kintone.app.getId()}).then(function(resp){
+      let recordList=resp.records;
+      let body={
+        app:kintone.app.getId(),
+        records:[]
       }
-      body.records.push({
-        id:recordList[i].$id,
-        record:{[subTable]:recordList[i][subTable]}
-      })
-    }
-    kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', body).then(function(resp){
-      console.log(resp);
+      for(let i in recordList){
+        let subTableValue=recordList[i][subTable].value;
+        for (let y in subTableValue){
+          subTableValue[y].value[fCode].value='';
+        }
+        body.records.push({
+          id:recordList[i].$id,
+          record:{[subTable]:recordList[i][subTable]}
+        })
+      }
+      kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', body).then(function(resp){
+        console.log(resp);
+      }).catch(function(err){
+        console.log(err);
+      });
+      // console.log(body);
     }).catch(function(err){
       console.log(err);
     });
-    // console.log(body);
-  }).catch(function(err){
-    console.log(err);
   });
-}
+})();
