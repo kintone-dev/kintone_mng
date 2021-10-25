f4fbe1631795a91d3cc1b5d1fd0e76a00e61ab5e
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-25 12:43:54 +0900

    Update report_adp.js

--- space/inventory/report_adp.js
+++ space/inventory/report_adp.js
@@ -151,8 +151,10 @@
         // 出荷予定数挿入
         event.record.forecastList.value[i].value.forecast_shipNum.value = totalShipNum;
         //リードタイム後残数
+        event.record.forecastList.value[i].value.afterLeadTimeStock.value = 0;
         event.record.forecastList.value[i].value.afterLeadTimeStock.value = (parseInt(event.record.forecastList.value[i].value.forecast_mStock.value) || 0) - (parseInt(totalArrivalNum) || 0) + (parseInt(totalShipNum) || 0);
         //差引残数
+        event.record.forecastList.value[i].value.remainingNum.value = 0;
         event.record.forecastList.value[i].value.remainingNum.value = (parseInt(event.record.forecastList.value[i].value.afterLeadTimeStock.value) || 0) - (parseInt(event.record.forecastList.value[i].value.mOrderingPoint.value) || 0);
       }
 
@@ -187,13 +189,10 @@
             for (var k in assItems) {
               if (assItems[k].mCode == assShipList.records[i].deviceList.value[j].value.mCode.value) {
                 if (assShipList.records[i].application_type.value == '新規申込' || assShipList.records[i].application_type.value == 'デバイス追加') {
-                  console.log('新規申込');
                   assItems[k].shipNum = parseInt(assItems[k].shipNum || 0) + parseInt(assShipList.records[i].deviceList.value[j].value.shipNum.value || 0);
                 } else if (assShipList.records[i].application_type.value == '故障交換（保証期間内）') {
-                  console.log('故障交換（保証期間内）');
                   assItems[k].inWarrantNum = parseInt(assItems[k].inWarrantNum || 0) + parseInt(assShipList.records[i].deviceList.value[j].value.shipNum.value || 0);
                 } else if (assShipList.records[i].application_type.value == '故障交換（保証期間外）') {
-                  console.log('故障交換（保証期間外）');
                   assItems[k].outWarrantNum = parseInt(assItems[k].outWarrantNum || 0) + parseInt(assShipList.records[i].deviceList.value[j].value.shipNum.value || 0);
                 }
               }
