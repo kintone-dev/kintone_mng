ab96e2c4d767e6d4be4b793bfdbdae644dc67daf
KM-kintone <keiichi.maeda@accel-lab.com>
2021-10-26 11:00:47 +0900

    edit

--- space/inventory/report_viewCustom.js
+++ space/inventory/report_viewCustom.js
@@ -148,23 +148,15 @@
   });
 
   //ソートボタン表示、処理
-  kintone.events.on(['app.record.edit.show', 'app.record.create.show'], function (event) {
+  kintone.events.on(['app.record.edit.show', 'app.record.create.show'], async function (event) {
     setBtn('itemSortBtn', '商品順');
     setBtn('locationSortBtn', '拠点順');
+    event.record.inventoryList.value = await sortItemTable(table, 'sys_code', true);
     $('#itemSortBtn').on('click', async function () {
       await startLoad();
       var eRecord = kintone.app.record.get();
       var table = eRecord.record.inventoryList.value;
       table = await sortItemTable(table, 'sys_code', true);
-      await new Promise(resolve => {
-        setTimeout(() => {
-          for (let i in eRecord.record.inventoryList.value) {
-            eRecord.record.inventoryList.value[i].value.mCode.lookup = true;
-          }
-          kintone.app.record.set(eRecord);
-          resolve();
-        }, 1000)
-      })
       await endLoad();
     });
     $('#locationSortBtn').on('click', async function () {
@@ -172,20 +164,8 @@
       var eRecord = kintone.app.record.get();
       var table = eRecord.record.inventoryList.value;
       table = await sortLocTable(table, 'sys_code', true);
-      await new Promise(resolve => {
-        setTimeout(() => {
-          for (let i in eRecord.record.inventoryList.value) {
-            eRecord.record.inventoryList.value[i].value.mCode.lookup = true;
-          }
-          kintone.app.record.set(eRecord);
-          resolve();
-        }, 1000)
-      })
       await endLoad();
     });
-    for (let i in event.record.inventoryList.value) {
-      event.record.inventoryList.value[i].value.mCode.lookup = true;
-    }
     return event;
   });
 
