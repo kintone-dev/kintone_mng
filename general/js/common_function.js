9ac7d257c69baabf26ada4c15ace8df675a0f308
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2021-11-01 14:57:32 +0900

    e

--- general/js/common_function.js
+++ general/js/common_function.js
@@ -499,7 +499,7 @@ function createStockJson(event, appId) {
 		}
 		if (event.nextStatus) {
 			if (event.nextStatus.value == '集荷待ち') {
-				var arrivalShipType = ['移動-販売', '移動-サブスク', '移動-拠点間', '移動-ベンダー'];
+				var arrivalShipType = ['移動-販売', '移動-サブスク', '移動-拠点間', '移動-ベンダー', '社内利用', '貸与', '修理・交換'];
 				for (let i in event.record.deviceList.value) {
 					/**
 					 * 出荷用json作成
@@ -534,7 +534,8 @@ function createStockJson(event, appId) {
 				}
 				return stockData;
 			} else if (event.nextStatus.value == '出荷完了') {
-				var arrivalShipType_dist = ['移動-販売', '移動-サブスク'];
+				var arrivalShipType_dist1 = ['移動-販売', '移動-サブスク'];
+				var arrivalShipType_dist2 = ['社内利用', '貸与', '修理・交換'];
 				var arrivalShipType_arr = ['移動-拠点間', '移動-ベンダー'];
 				for (let i in event.record.deviceList.value) {
 					// 出荷情報を作成
@@ -545,11 +546,19 @@ function createStockJson(event, appId) {
 						'stockNum': event.record.deviceList.value[i].value.shipNum.value
 					};
 					stockData.ship.push(stockShipBody);
-					if (arrivalShipType_dist.includes(event.record.shipType.value)) { // 出荷区分がarrivalShipType_distに含まれる場合のみ入荷情報を作成
+					if (arrivalShipType_dist1.includes(event.record.shipType.value)) { // 出荷区分がarrivalShipType_dist1に含まれる場合のみ入荷情報を作成
 						var stockArrBody = {
 							'arrOrShip': 'arr',
 							'devCode': event.record.deviceList.value[i].value.mCode.value,
 							'uniCode': 'distribute',
+							'stockNum': event.record.deviceList.value[i].value.shipNum.value
+						};
+						stockData.arr.push(stockArrBody)
+					} else if (arrivalShipType_dist2.includes(event.record.shipType.value)) { // 出荷区分がarrivalShipType_dist2に含まれる場合のみ入荷情報を作成
+						var stockArrBody = {
+							'arrOrShip': 'arr',
+							'devCode': event.record.deviceList.value[i].value.mCode.value,
+							'uniCode': 'reuse(incomp)',
 							'stockNum': event.record.deviceList.value[i].value.shipNum.value
 						};
 						stockData.arr.push(stockArrBody)
