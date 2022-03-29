b20b96bdd5a00c910a3ea364ead0f1c5dfe0cafe
kintone-mng <89760706+kintone-mng@users.noreply.github.com>
2022-03-30 08:20:52 +0900

    Update common_function.js

--- general/js/common_function.js
+++ general/js/common_function.js
@@ -352,7 +352,7 @@ function renew_sNumsInfo_alship(shipRecord, snTableName){
 		else if(checkType == 'internal' && snRecord.sState.value == '再生品') checkSNstatus = 'recycle';
 		else if(checkType == 'internal' && snRecord.sState.value == '社内用') checkSNstatus = 'recycle';
 		else if(checkType == 'all' && snRecord.sState.value == '新品') checkSNstatus = 'newship';
-		else if(checkType == 'all' && snRecord.sState.value != '再生品') checkSNstatus = 'recycle';
+		else if(checkType == 'all' && snRecord.sState.value != '新品') checkSNstatus = 'recycle';
 		else{
 			console.log('stop Serial control');
 			return {result: false,  error: {target: snRecord.sNum.value, code: 'sn_cannotuse'}};
